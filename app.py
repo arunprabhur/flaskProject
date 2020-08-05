dc8fe9ecba05f279f52eb74e200fcae5a6626b3a
prabhu_arun10 <prabhu.arun10@gmail.com>
2020-08-05 13:14:43 +0000

    Commit

---
+++ app.py
@@ -0,0 +1,108 @@
+from flask import Flask, render_template, request
+import sqlalchemy
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def student():
+    return render_template('student.html')
+
+@app.route('/showall')
+def displayall():
+    Data =[]
+    with db.connect() as conn:
+        # Execute the query and fetch all results
+        tabledata= conn.execute(
+            "SELECT Issuetype, IssueDescription FROM Issuetb "
+        ).fetchall()
+    for row in tabledata:
+        Data.append({"Issuetype": row[0], "IssueDescription": row[1]})
+    return render_template("showall.html", data=Data)
+
+
+@app.route('/result', methods=['POST', 'GET'])
+def result():
+    if request.method == 'POST':
+        result = request.form
+        Category = request.form.get('Category')
+        Description = request.form.get('Description')
+
+        stmt = sqlalchemy.text(
+            "INSERT INTO Issuetb (Issuetype, IssueDescription)" " VALUES (:Category, :Description)")
+        with db.connect() as conn:
+            conn.execute(stmt, Category=Category, Description=Description)
+
+        return render_template("result.html", result=result)
+
+
+def init_connection_engine():
+    db_config = {
+        "pool_size": 5,
+        "max_overflow": 2,
+        "pool_timeout": 30,  # 30 seconds
+        "pool_recycle": 1800,  # 30 minutes
+    }
+
+    return init_tcp_connection_engine(db_config)
+
+
+def init_tcp_connection_engine(db_config):
+    db_user = "root"
+    db_pass = "root"
+    db_name = "mysqldb"
+    db_host = "35.224.40.149:3306"
+
+    # Extract host and port from db_host
+    host_args = db_host.split(":")
+    db_hostname, db_port = host_args[0], int(host_args[1])
+
+    pool = sqlalchemy.create_engine(
+        # Equivalent URL:
+        # mysql+pymysql://<db_user>:<db_pass>@<db_host>:<db_port>/<db_name>
+        sqlalchemy.engine.url.URL(
+            drivername="mysql+pymysql",
+            username=db_user,  # e.g. "my-database-user"
+            password=db_pass,  # e.g. "my-database-password"
+            host=db_hostname,  # e.g. "127.0.0.1"
+            port=db_port,  # e.g. 3306
+            database=db_name,  # e.g. "my-database-name"
+        ),
+        # ... Specify additional properties here.
+        # [END cloud_sql_mysql_sqlalchemy_create_tcp]
+        **db_config
+        # [START cloud_sql_mysql_sqlalchemy_create_tcp]
+    )
+    # [END cloud_sql_mysql_sqlalchemy_create_tcp]
+
+    return pool
+
+
+def init_unix_connection_engine(db_config):
+    # [START cloud_sql_mysql_sqlalchemy_create_socket]
+    # Remember - storing secrets in plaintext is potentially unsafe. Consider using
+    # something like https://cloud.google.com/secret-manager/docs/overview to help keep
+    # secrets secret.
+
+
+    return ""
+
+
+db = init_connection_engine()
+
+
+@app.route('/db')
+def create_tables():
+    # Create tables (if they don't already exist)
+    with db.connect() as conn:
+        conn.execute(
+            "CREATE TABLE IF NOT EXISTS Issuetb "
+            "( vote_id SERIAL NOT NULL, time_cast timestamp NOT NULL, "
+            "Issuetype CHAR(20) NOT NULL, IssueDescription CHAR(20), PRIMARY KEY (vote_id) );"
+        )
+    return "Success db"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

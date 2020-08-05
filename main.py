dd67121050d72986078cb4dd78268f13d652e980
arunprabhur <prabhu.arun10@gmail.com>
2020-08-05 19:13:10 +0530

    Update main.py
    update db config

--- main.py
+++ main.py
@@ -37,6 +37,8 @@ def result():
         return render_template("result.html", result=result)
 
 
+
+
 def init_connection_engine():
     db_config = {
         "pool_size": 5,
@@ -45,14 +47,21 @@ def init_connection_engine():
         "pool_recycle": 1800,  # 30 minutes
     }
 
-    return init_tcp_connection_engine(db_config)
+    if os.environ.get("DB_HOST"):
+        return init_tcp_connection_engine(db_config)
+    else:
+        return init_unix_connection_engine(db_config)
 
 
 def init_tcp_connection_engine(db_config):
-    db_user = "root"
-    db_pass = "root"
-    db_name = "mysqldb"
-    db_host = "35.224.40.149:3306"
+    # [START cloud_sql_mysql_sqlalchemy_create_tcp]
+    # Remember - storing secrets in plaintext is potentially unsafe. Consider using
+    # something like https://cloud.google.com/secret-manager/docs/overview to help keep
+    # secrets secret.
+    db_user = os.environ["DB_USER"]
+    db_pass = os.environ["DB_PASS"]
+    db_name = os.environ["DB_NAME"]
+    db_host = os.environ["DB_HOST"]
 
     # Extract host and port from db_host
     host_args = db_host.split(":")
@@ -84,12 +93,41 @@ def init_unix_connection_engine(db_config):
     # Remember - storing secrets in plaintext is potentially unsafe. Consider using
     # something like https://cloud.google.com/secret-manager/docs/overview to help keep
     # secrets secret.
+    db_user = os.environ["DB_USER"]
+    db_pass = os.environ["DB_PASS"]
+    db_name = os.environ["DB_NAME"]
+    db_socket_dir = os.environ.get("DB_SOCKET_DIR", "/cloudsql")
+    cloud_sql_connection_name = os.environ["CLOUD_SQL_CONNECTION_NAME"]
+
+    pool = sqlalchemy.create_engine(
+        # Equivalent URL:
+        # mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=<socket_path>/<cloud_sql_instance_name>
+        sqlalchemy.engine.url.URL(
+            drivername="mysql+pymysql",
+            username=db_user,  # e.g. "my-database-user"
+            password=db_pass,  # e.g. "my-database-password"
+            database=db_name,  # e.g. "my-database-name"
+            query={
+                "unix_socket": "{}/{}".format(
+                    db_socket_dir,  # e.g. "/cloudsql"
+                    cloud_sql_connection_name)  # i.e "<PROJECT-NAME>:<INSTANCE-REGION>:<INSTANCE-NAME>"
+            }
+        ),
+        # ... Specify additional properties here.
+
+        # [END cloud_sql_mysql_sqlalchemy_create_socket]
+        **db_config
+        # [START cloud_sql_mysql_sqlalchemy_create_socket]
+    )
+    # [END cloud_sql_mysql_sqlalchemy_create_socket]
+
+    return pool
 
 
-    return ""
-
-
+# The SQLAlchemy engine will help manage interactions, including automatically
+# managing a pool of connections to your database
 db = init_connection_engine()
+
 
 
 @app.route('/db')

faa7d4f6a985f76adfb395cfebb3c16b8c58c0d4
prabhu_arun10 <prabhu.arun10@gmail.com>
2020-08-05 14:03:53 +0000

    Added webhook code

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template, request
+from __future__ import print_function
+from urllib.parse import urlparse, urlencode
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
+from flask import Flask, render_template, request, make_response
 import sqlalchemy
+import json
 import os
+import requests as core_requests
 
 app = Flask(__name__)
 
@@ -9,12 +15,13 @@ app = Flask(__name__)
 def student():
     return render_template('student.html')
 
+
 @app.route('/showall')
 def displayall():
-    Data =[]
+    Data = []
     with db.connect() as conn:
         # Execute the query and fetch all results
-        tabledata= conn.execute(
+        tabledata = conn.execute(
             "SELECT Issuetype, IssueDescription FROM Issuetb "
         ).fetchall()
     for row in tabledata:
@@ -37,6 +44,43 @@ def result():
         return render_template("result.html", result=result)
 
 
+def init_tcp_connection_engine_local():
+    db_config = {
+        "pool_size": 5,
+        "max_overflow": 2,
+        "pool_timeout": 30,  # 30 seconds
+        "pool_recycle": 1800,  # 30 minutes
+    }
+    db_user = "root"
+    db_pass = "root"
+    db_name = "mysqldb"
+    db_host = "35.224.40.149:3306"
+
+    # Extract host and port from db_host
+    host_args = db_host.split(":")
+    db_hostname, db_port = host_args[0], int(host_args[1])
+
+    pool = sqlalchemy.create_engine(
+        # Equivalent URL:
+        # mysql+pymysql://<db_user>:<db_pass>@<db_host>:<db_port>/<db_name>
+        sqlalchemy.engine.url.URL(
+            drivername="mysql+pymysql",
+            username=db_user,  # e.g. "my-database-user"
+            password=db_pass,  # e.g. "my-database-password"
+            host=db_hostname,  # e.g. "127.0.0.1"
+            port=db_port,  # e.g. 3306
+            database=db_name,  # e.g. "my-database-name"
+        ),
+        # ... Specify additional properties here.
+        # [END cloud_sql_mysql_sqlalchemy_create_tcp]
+        **db_config
+        # [START cloud_sql_mysql_sqlalchemy_create_tcp]
+    )
+    # [END cloud_sql_mysql_sqlalchemy_create_tcp]
+
+    return pool
+
+
 def init_connection_engine():
     db_config = {
         "pool_size": 5,
@@ -45,14 +89,21 @@ def init_connection_engine():
         "pool_recycle": 1800,  # 30 minutes
     }
 
-    return init_tcp_connection_engine(db_config)
+    if os.environ.get("DB_HOST"):
+        return init_tcp_connection_engine(db_config)
+    else:
+        return init_unix_connection_engine(db_config)
 
 
 def init_tcp_connection_engine(db_config):
-    db_user = "root"
-    db_pass = "root"
-    db_name = "mysqldb"
-    db_host = "35.224.40.149:3306"
+    # [START cloud_sql_mysql_sqlalchemy_create_tcp]
+    # Remember - storing secrets in plaintext is potentially unsafe. Consider using
+    # something like https://cloud.google.com/secret-manager/docs/overview to help keep
+    # secrets secret.
+    db_user = os.environ["DB_USER"]
+    db_pass = os.environ["DB_PASS"]
+    db_name = os.environ["DB_NAME"]
+    db_host = os.environ["DB_HOST"]
 
     # Extract host and port from db_host
     host_args = db_host.split(":")
@@ -84,12 +135,38 @@ def init_unix_connection_engine(db_config):
     # Remember - storing secrets in plaintext is potentially unsafe. Consider using
     # something like https://cloud.google.com/secret-manager/docs/overview to help keep
     # secrets secret.
+    db_user = os.environ["DB_USER"]
+    db_pass = os.environ["DB_PASS"]
+    db_name = os.environ["DB_NAME"]
+    db_socket_dir = os.environ.get("DB_SOCKET_DIR", "/cloudsql")
+    cloud_sql_connection_name = os.environ["CLOUD_SQL_CONNECTION_NAME"]
+
+    pool = sqlalchemy.create_engine(
+        # Equivalent URL:
+        # mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=<socket_path>/<cloud_sql_instance_name>
+        sqlalchemy.engine.url.URL(
+            drivername="mysql+pymysql",
+            username=db_user,  # e.g. "my-database-user"
+            password=db_pass,  # e.g. "my-database-password"
+            database=db_name,  # e.g. "my-database-name"
+            query={
+                "unix_socket": "{}/{}".format(
+                    db_socket_dir,  # e.g. "/cloudsql"
+                    cloud_sql_connection_name)  # i.e "<PROJECT-NAME>:<INSTANCE-REGION>:<INSTANCE-NAME>"
+            }
+        ),
+        # ... Specify additional properties here.
+
+        # [END cloud_sql_mysql_sqlalchemy_create_socket]
+        **db_config
+        # [START cloud_sql_mysql_sqlalchemy_create_socket]
+    )
+    # [END cloud_sql_mysql_sqlalchemy_create_socket]
+
+    return pool
 
 
-    return ""
-
-
-db = init_connection_engine()
+db = init_tcp_connection_engine_local()
 
 
 @app.route('/db')
@@ -102,6 +179,47 @@ def create_tables():
             "Issuetype CHAR(20) NOT NULL, IssueDescription CHAR(20), PRIMARY KEY (vote_id) );"
         )
     return "Success db"
+
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    print("Request:")
+    print(json.dumps(req, indent=4))
+
+    res = make_openweathermap_request(req)
+
+    res = json.dumps(res, indent=4)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+
+def make_openweathermap_request(req):
+    result = req.get("queryResult")
+    parameters = result.get("parameters")
+    city = parameters.get("geo-city")
+    if city is None:
+        return None
+
+    print('https://api.openweathermap.org/data/2.5/weather?q={}&appid=4caf7420789e9f763a81fbc58dc34e31'.format(city))
+
+    try:
+        r = core_requests.get(
+            'https://api.openweathermap.org/data/2.5/weather?q={}&appid=4caf7420789e9f763a81fbc58dc34e31'.format(city))
+        current_temp = r.json()['main']['temp']
+        current_temp = float(current_temp) - 273.0
+        current_temp = int(current_temp)
+
+        speech_response = 'Current temperature in {} is {} Celsius'.format(city, current_temp)
+    except Exception as e:
+        speech_response = 'Unable to get temperature of {}'.format(city)
+
+    return {
+        "fulfillmentText": speech_response,
+        "source": "Yahoo Weather"
+    }
 
 
 if __name__ == '__main__':
